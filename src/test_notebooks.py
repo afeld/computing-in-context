a3335bbc978ab6ab3a288b45ef94b08bbf70690f
Aidan Feldman <aidan.feldman@gmail.com>
2024-10-25 12:18:04 -0400

    use absolute URLs for images

--- src/test_notebooks.py
+++ src/test_notebooks.py
@@ -71,10 +71,15 @@ def test_nested_lists(file):
 
 
 def check_link(token: Token, parent: Token = None):
+    url = None
     if token.type == "link_open":
-        href = token.attrGet("href")
+        url = token.attrGet("href")
+    if token.type == "image":
+        url = token.attrGet("src")
+
+    if url:
         # escaped Jinja2 tags
-        if not re.match(r"http|#|%7B%7B", href):
+        if not re.match(r"http|#|%7B%7B", url):
             source = parent.content if parent else token.content
             assert False, f"Link should be absolute. Text:\n\n{source}\n"
 
