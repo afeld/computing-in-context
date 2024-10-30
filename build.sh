2fa7730dc3b8219c7a526247091828af873945e7
Aidan Feldman <aidan.feldman@gmail.com>
2024-10-30 00:30:45 -0400

    have the build script also check for broken links

--- build.sh
+++ build.sh
@@ -5,6 +5,8 @@ set -x
 
 
 jupyter-book clean .
-jupyter-book build .
+# https://jupyterbook.org/en/stable/advanced/sphinx.html#enable-a-custom-sphinx-builder-from-the-cli
+# https://sublime-and-sphinx-guide.readthedocs.io/en/latest/references.html#test-external-links
+jupyter-book build --builder=linkcheck .
 
 pytest
