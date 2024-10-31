e1fbc9394b083e24cd270f25c7f3440f369ccc2c
Aidan Feldman <aidan.feldman@gmail.com>
2024-10-31 00:40:11 -0400

    fail on build warnings

--- build.sh
+++ build.sh
@@ -4,10 +4,10 @@ set -e
 set -x
 
 
-jupyter-book clean .
+# https://jupyterbook.org/en/stable/content/references.html#check-for-missing-references
 # https://jupyterbook.org/en/stable/advanced/sphinx.html#enable-a-custom-sphinx-builder-from-the-cli
 # https://sublime-and-sphinx-guide.readthedocs.io/en/latest/references.html#test-external-links
-jupyter-book build --builder=linkcheck .
+jupyter-book build --all -W -n --keep-going --builder=linkcheck .
 
 # check for Jinja tags, copied from Python for Public Policy
 git ls-files '*.md' '*.ipynb' | xargs grep -q -E '\{\{|\{%' && exit 1 || echo "No Jinja tags found"
