bc9244c696e4822ffd4cbf73b96486ba111a3e6d
Aidan Feldman <aidan.feldman@gmail.com>
2024-11-21 00:26:34 -0500

    confirm certain words aren't present

--- src/test_files.py
+++ src/test_files.py
@@ -8,10 +8,28 @@ JINJA_EXPRESSION = re.compile(r"{{ ?\w+ ?}}")
 files = glob("*.ipynb") + glob("*.md")
 
 
+def get_content(file: str):
+    with open(file) as f:
+        return f.read()
+
+
 @pytest.mark.parametrize("file", files)
 def test_no_jinja(file):
-    with open(file) as f:
-        content = f.read()
-
+    content = get_content(file)
     assert JINJA_EXPRESSION.search(content) is None, "Jinja expression found"
     assert "{%" not in content, "Jinja statement found"
+
+
+@pytest.mark.parametrize("file", files)
+def test_no_python_public_policy_stuff(file):
+    # https://github.com/afeld/python-public-policy/blob/main/extras/lib/school.py
+    words = [
+        "Brightspace",
+        "JupyterHub",
+        "Reader",
+        "grader",
+    ]
+
+    content = get_content(file)
+    for word in words:
+        assert word not in content
