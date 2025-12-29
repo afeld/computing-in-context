2ea1d68cf1cef08e761e42e2e6910ef9350ed993
Aidan Feldman <aidan.feldman@gmail.com>
2025-12-29 04:07:35 -0500

    add type hints to nb_helper

--- src/nb_helper.py
+++ src/nb_helper.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 
 import nbformat
+from nbformat import NotebookNode
 
 PAGES_DIR = Path(__file__).parent.parent / "pages"
 
@@ -12,15 +13,15 @@ def read_notebook(notebook_path: Path):
     return nbformat.read(notebook_path, as_version=4)
 
 
-def is_markdown(cell):
+def is_markdown(cell: NotebookNode):
     return cell.cell_type == "markdown"
 
 
-def is_h1(cell):
+def is_h1(cell: NotebookNode):
     return is_markdown(cell) and cell.source.startswith("# ")
 
 
-def is_magic(source):
+def is_magic(source: str):
     return source.startswith("%%")
 
 
@@ -28,10 +29,10 @@ def is_system_command(source: str):
     return source.startswith("!")
 
 
-def is_code_cell(cell):
+def is_code_cell(cell: NotebookNode):
     return cell.cell_type == "code"
 
 
-def is_python(cell):
+def is_python(cell: NotebookNode):
     source = cell.source
     return is_code_cell(cell) and not (is_magic(source) or is_system_command(source))
